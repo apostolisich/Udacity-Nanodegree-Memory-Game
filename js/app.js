eb53aadfdce05078237ec5bf6228773ea785a857
tolisich <it14126@uom.edu.gr>
2018-03-24 00:49:07 +0200

    Working solution
    type: refactor
    
    Fixed the issue with the card that was not showing after clicking on it for a second time after being selected as a wrong card. Also added the timer functionality.

--- js/app.js
+++ js/app.js
@@ -51,8 +51,8 @@ initiateArray(myArray);
 // Restart button listener
 document.getElementsByClassName("restart")[0].addEventListener("click", function() {
     let restart = confirm("Are you sure that you want to start over?");
-    if(restart == true){
-        restart();
+    if(restart === true){
+        restartGame();
     }
 });
 
@@ -60,40 +60,41 @@ document.getElementsByClassName("restart")[0].addEventListener("click", function
 document.getElementById("play-again").addEventListener("click", function(e) {
     document.getElementById("game-div").style.visibility = "visible";
     document.getElementById("result-div").style.display = "none";
-    restart();
+    restartGame();
 });
 
 // Card listener
 document.getElementsByClassName("deck")[0].addEventListener("click", function(e) {
   if(e.target && e.target.matches("li") && e.target.className != "card match") {
     let currentCard = e.target;
-    currentCard.classList.remove("buzz");
-    displayCard(currentCard);
+    currentCard.classList.remove("buzz"); 
 
     if(previousCard != null) {
         incrementMoveNo();
-        if (previousCard.innerHTML == currentCard.innerHTML) {
-            console.log("show");
+        moveRating(document.getElementsByClassName("moves")[0].innerHTML);
+        if(previousCard.innerHTML == currentCard.innerHTML) {
             lockCards(previousCard, currentCard);
             matchedCardsNo += 2;
+
+            if(matchedCardsNo === 16) {
+                setTimeout(function() { 
+                    document.getElementById("game-div").style.visibility = "hidden";
+                document.getElementById("result-div").style.display = "inline-block";
+        
+                document.getElementById("moveCount").innerHTML = document.getElementsByClassName("moves")[0].innerHTML;
+                document.getElementById("starCount").innerHTML = document.getElementsByClassName("fa fa-star").length;
+                }, 1000);
+            }
             previousCard = null;
         } else {
-            console.log("hide");
             hideCards(previousCard, currentCard);
             previousCard = null;
         }
     } else {
+        displayCard(currentCard);
         previousCard = currentCard;
-    }
+    } 
     
-    if(matchedCardsNo === 4) {
-        document.getElementById("game-div").style.visibility = "hidden";
-        document.getElementById("result-div").style.display = "inline-block";
-
-        document.getElementById("moveCount").innerHTML = document.getElementsByClassName("moves")[0].innerHTML;
-        document.getElementById("starCount").innerHTML = document.getElementsByClassName("fa fa-star").length;
-
-    }
     /*addCard(e.target.innerHTML);
 
     if(openCardsArray.length > 1){
@@ -110,9 +111,7 @@ document.getElementsByClassName("deck")[0].addEventListener("click", function(e)
 });
 
 function displayCard(selectedElement){
-    console.log("card-display");
-    selectedElement.classList.add("open");
-    selectedElement.classList.add("show");
+    selectedElement.className += " open show";
 }
 
 /*function addCard(card) {
@@ -120,36 +119,66 @@ function displayCard(selectedElement){
 }*/
 
 function lockCards(matchCard1, matchCard2) {
-    matchCard1.classList.remove("open");
-    matchCard1.classList.remove("show");
-    matchCard1.classList.add("match");
-
-    matchCard2.classList.remove("open");
-    matchCard2.classList.remove("show");
-    matchCard2.classList.add("match");
+    matchCard1.className = "card match grow";
+    matchCard2.className = "card match grow";
+    setTimeout(function(){ matchCard1.classList.remove("grow"); }, 500);
+    setTimeout(function(){ matchCard2.classList.remove("grow"); }, 500);
 }
 
 function hideCards(matchCard1, matchCard2) {
-    matchCard1.classList.remove("open");
-    matchCard1.classList.remove("show");
-    matchCard2.classList.remove("open");
-    matchCard2.classList.remove("show");
-
-    matchCard1.classList.add("buzz");
-    matchCard2.classList.add("buzz");
+    matchCard1.className = "card buzz";
+    matchCard2.className = "card buzz"
+    setTimeout(function(){ matchCard1.classList.remove("buzz"); }, 1000);
+    setTimeout(function(){ matchCard2.classList.remove("buzz"); }, 1000);
 }
 
-function incrementMoveNo () {
+function incrementMoveNo() {
     document.getElementsByClassName("moves")[0].innerHTML = parseInt(document.getElementsByClassName("moves")[0].innerHTML) + 1;
 }
 
-function restart() {
+function moveRating(moveCount) {
+    let starList = document.getElementsByClassName("fa fa-star");
+    if(moveCount == 4) {
+        starList[2].className = "fa fa-star-o";
+    } else if(moveCount == 7) {
+        starList[1].className = "fa fa-star-o";
+    } else if(moveCount == 10) {
+        starList[0].className = "fa fa-star-o";
+    }
+}
+
+function resetRating() {
+    let starList = document.getElementsByClassName("fa fa-star-o");
+    starList[0].className = "fa fa-star";
+    starList[0].className = "fa fa-star";
+    starList[0].className = "fa fa-star";
+}
+
+function restartGame() {
     let deck = document.getElementsByClassName("deck")[0];
-    while (deck.lastChild) {
+    while(deck.lastChild) {
         deck.removeChild(deck.lastChild);
     }
 
     document.getElementsByClassName("moves")[0].innerHTML = 0;
     matchedCardsNo = 0;
     initiateArray(myArray);
+    resetRating();
 }
+
+function timer() {
+    let start = Date.now();
+    let minutes = 0;
+    setInterval(function() {
+        let delta = Date.now() - start; // milliseconds elapsed since start
+
+        let seconds = Math.floor(delta / 1000);
+        if(seconds === 60){
+            minutes++;
+            start = Date.now();
+        }
+        document.getElementById("timer").innerHTML = (minutes < 10 ? "0"+ minutes : minutes) + ":" + (seconds < 10 ? "0"+ seconds : seconds);
+    }, 100);
+}
+
+timer();
